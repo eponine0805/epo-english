878c0a0fdb8e634d6766bcb8118a94e84254bc54
eponine0805 <umaumasei1rou@gmail.com>
2026-03-19 17:44:48 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Hello, Epo English Studio!</h1>
+      <p>Firebaseの設定も完了！ここからアプリ開発スタートです！</p>
+    </div>
+  );
+}
+
+export default App;
